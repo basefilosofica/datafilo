f3afe8c8e6c15da24c664cfaa2b4b87e99d0a6b9
Hiperlogado <hiperlogado@gmail.com>
2024-07-16 17:05:04 -0300

    Primeiro Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# datafilo
+# dataFilo - _Projeto Base Filosófica_
+
+Scripts para a análise dos dados da Pós-Graduação Stricto Sensu em Filosofia no Brasil, disponibilizados no âmbito do Projeto Base Filosófica.
+
+## Sobre o dataFilo
+
+A análise de dados é um dos eixos centrais do __Projeto Base Filosófica__, que oferece suporte à pesquisa acadêmica em filosofia no Brasil, através da indexação e disponibilização de produções como __Artigos__, __Teses__ e __Dissertações__, e da produção de conhecimento sobre técnicas, métodos e recursos úteis ao desenvolvimento dos estudos.
+
+O __dataFilo__ objetiva ptofuxit análises com foco na realidade da Pós-Graduação Stricto Sensu em Filosofia no Brasil, caracterizando do cenário dos estudos e das pesquisa, desde as suas condições até a publicização de seus resultados.
+
+## Fontes de dados
+
+Atualmente, utilizamos como fontes principais as bases disponíveis no site de dados abertos da Capes (https://dadosabertos.capes.gov.br/). Os nomes das bases de dados da Capes foram conservados como no original, mas os arquivos foram compactados para GZ, cujo tamanho reduzido facilita o transporte, ao mesmo tempo, que não impede de ser carregado a partir da função _fread()_, da biblioteca __data.tables__.
+
+Outra fonte é a coleta direta de informações disponíveis nos sites de periódicos acadêmicos, monitorados a partir dos metadados capturados na interface gerada pelo sistema Open Journal System (OJS).
+
+## Produção
+
+Utiliza-se a __Linguagem R__ como padrão para o processo de Extração, Tratamento e Carga (ETL) dos dados. A saída final é direcionada para __arquivos CSV__, contendo as visões necessárias às análises.
+
+## Produtos
+
+A partir dos arquivos CSV diversas técnicas e métodos estatísticos podem ser aplicados. Para ver o resultado de algumas análises acesse a página do __dataFilo__, disponível no site Base Filosófica (https://basefilosofica.com.br/datafilo).
---
+++ etl/teses_e_dissertacoes.R
@@ -0,0 +1,63 @@
+library(data.table)
+library(plyr)
+
+dir <- "fontes/"
+
+anos <- c(1987:2012)
+
+ate_2012 <- data.frame()
+
+for(ano in anos){
+
+  dt <- fread(paste0(dir,"dados_",as.character(ano),'.gz'), encoding = 'Latin-1')
+  if(nrow(dt)>0){
+    dt <- subset(dt,select=c(AnoBase,Uf,SiglaIes,CodigoPrograma,NomePrograma,AreaConhecimentoCodigo,AreaConhecimento,GrandeAreaCodigo,GrandeAreaDescricao,Nivel))
+    ate_2012 <- rbind.fill(ate_2012,dt)
+  }
+
+}
+
+arquivos <- c("br-capes-bdt-2013a2016-2017-12-01_2013.gz",
+"br-capes-bdt-2013a2016-2017-12-01_2014.gz",
+"br-capes-bdt-2013a2016-2017-12-01_2015.gz",
+"br-capes-bdt-2013a2016-2017-12-01_2016.gz",
+"br-capes-bdt-2017a2020-2021-12-03_2017.gz",
+"br-capes-bdt-2017a2020-2021-12-03_2018.gz",
+"br-capes-bdt-2017a2020-2021-12-03_2019.gz",
+"br-capes-bdt-2017a2020-2021-12-03_2020.gz",
+"br-capes-bdt-2021-2023-10-31.gz",
+"br-capes-bdt-2022-2023-10-31.gz")
+
+desde_2013 <- data.frame()
+
+for(arquivo in arquivos){
+
+  print(arquivo)
+  dt <- fread(paste0(dir,arquivo), encoding = 'Latin-1')
+  if(nrow(dt)>0){
+    dt <- subset(dt,select=c(AN_BASE,SG_UF_IES,SG_ENTIDADE_ENSINO,CD_PROGRAMA,NM_PROGRAMA,CD_AREA_CONHECIMENTO,NM_AREA_CONHECIMENTO,CD_GRANDE_AREA_CONHECIMENTO,NM_GRANDE_AREA_CONHECIMENTO,NM_GRAU_ACADEMICO))
+    desde_2013 <- rbind.fill(desde_2013,dt)
+  }
+
+}
+
+base <- rbind.fill(
+    setNames(
+        subset(ate_2012,select=c(AnoBase,Uf,SiglaIes,CodigoPrograma,NomePrograma,AreaConhecimentoCodigo,AreaConhecimento,GrandeAreaCodigo,GrandeAreaDescricao,Nivel)),
+        c("ano","uf_sg","ies_sg","programa_cd","programa","area_cd","area","grande_area_cd","grande_area","grau")
+    ),
+    setNames(
+        subset(desde_2013,select=c(AN_BASE,SG_UF_IES,SG_ENTIDADE_ENSINO,CD_PROGRAMA,NM_PROGRAMA,CD_AREA_CONHECIMENTO,NM_AREA_CONHECIMENTO,CD_GRANDE_AREA_CONHECIMENTO,NM_GRANDE_AREA_CONHECIMENTO,NM_GRAU_ACADEMICO)),
+        c("ano","uf_sg","ies_sg","programa_cd","programa","area_cd","area","grande_area_cd","grande_area","grau")
+    )
+)
+
+base$programa <- toupper(base$programa)
+base$grau <- toupper(base$grau)
+base$programa <- ifelse(base$grande_area=="CIÊNCIAS HUMANAS",base$programa,"Outros")
+base$area <- ifelse(base$grande_area=="CIÊNCIAS HUMANAS",base$area,"Outras")
+base$grande_area <- ifelse(base$grande_area=="CIÊNCIAS HUMANAS",base$grande_area,"Outras")
+
+view <- setNames(dcast(data.table(base),ano+grau+uf_sg+ies_sg+grande_area+area+programa~""),c("ano","grau","uf_sg","ies_sg","grande_area","area","programa","n_producoes"))
+
+write.csv2(view,"tabelas/teses_e_dissertacoes.csv",row.names=F,na="")
